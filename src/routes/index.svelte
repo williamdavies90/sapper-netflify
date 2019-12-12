cb72f68d5dc17fab0f284b3224b2f940b5d927b8
willpc <william@syspal.com>
2019-12-12 14:30:26 +0000

    Configured the site to run the Netlify CMS

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,18 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  });
+</script>
 <style>
 	h1, figure, p {
 		text-align: center;
@@ -34,6 +49,7 @@
 
 <svelte:head>
 	<title>Sapper project template</title>
+	<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
 <h1>Great test!</h1>
