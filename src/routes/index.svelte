c5b466a7693ed6770cd323dc78f1c252d5316740
William Davies <williamdavies@williams-imac.home>
2019-12-26 18:01:57 +0000

    minor commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,4 @@
-<script>
-	
-	import Row from '../components/Row.svelte';
-	import Col from '../components/Col.svelte';
-	import { onMount } from 'svelte';
-	onMount(() => {
-		const second = 1000,
-		minute = second * 60,
-		hour = minute * 60,
-		day = hour * 24;
 
-		let countDown = new Date('June 27, 2020 00:00:00').getTime(),
-		x = setInterval(function() {
-
-		let now = new Date().getTime(),
-		distance = countDown - now;
-
-		document.getElementById('days').innerText = Math.floor(distance / (day)),
-		document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-		document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-		document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-		}, second);
-	});
-</script>
 <style>
 	ul {
 		padding-left: 0px;
@@ -49,8 +26,14 @@
 <svelte:head>
 	<title>Lucy &amp; Tim</title>
 </svelte:head>
-<img class="mx-auto d-block img-fluid" src="../tim-and-lucy-mobile.jpg" alt="">
+
     
+<Carousel perPage={{ 800: 1, 500: 1 }}>
+  <div class="slide-content"><img class="mx-auto d-block img-fluid" src="../tim-and-lucy-mobile.jpg" alt=""></div>
+  <div class="slide-content"><img class="mx-auto d-block img-fluid" src="../tim-and-lucy-mobile.jpg" alt=""></div>
+  <div class="slide-content"><img class="mx-auto d-block img-fluid" src="../tim-and-lucy-mobile.jpg" alt=""></div>
+  <div class="slide-content"><img class="mx-auto d-block img-fluid" src="../tim-and-lucy-mobile.jpg" alt=""></div>
+</Carousel>
 
 <ul class="text-center">
     <li><span id="days"></span>days</li>
@@ -58,3 +41,30 @@
     <li><span id="minutes"></span>Minutes</li>
     <li><span id="seconds"></span>Seconds</li>
 </ul>
+<script>
+	
+	import Row from '../components/Row.svelte';
+	import Col from '../components/Col.svelte';
+	import { onMount } from 'svelte';
+	import Carousel from '@beyonk/svelte-carousel'
+	
+	onMount(() => {
+		
+		const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
+
+		let countDown = new Date('June 27, 2020 00:00:00').getTime(),
+		x = setInterval(function() {
+
+		let now = new Date().getTime(),
+		distance = countDown - now;
+
+		document.getElementById('days').innerText = Math.floor(distance / (day)),
+		document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+		document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+		document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+		}, second);
+	});
+</script>
