df1572913562e0512ce763fedce0998ed284c6e9
William Davies <williamdavies@williams-imac.home>
2020-01-10 15:22:13 +0000

    form commit 2

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -7,12 +7,7 @@
 		{ label: 'Address', value: 'SYSPAL, Cockshutt Lane' },
 		{ label: 'Telephone', value: '07527564964' }
   ];
-  let invite = 0;
-  let guests = 1;
-  function addGuest(event) {
-    event.preventDefault();
-    guests++;
-  }
+  let invite = false;
 </script>
 
 <svelte:head>
@@ -21,23 +16,6 @@
   
 <form name="wedding-rsvp-v1" method="POST" data-netlify="true">
   <input type="hidden" name="form-name" value="wedding-rsvp-v1">
-  <div class="form-group row">
-    <label class="col-4 col-form-label">Type of guest</label> 
-    <div class="col-8">
-      <div class="custom-controls-stacked">
-        <div class="custom-control custom-radio">
-          <input bind:group={invite} name="type_of_guest" id="type_0" type="radio" required="required" class="custom-control-input" value={1}> 
-          <label for="type_0" class="custom-control-label">Day and Evening</label>
-        </div>
-      </div>
-      <div class="custom-controls-stacked">
-        <div class="custom-control custom-radio">
-          <input bind:group={invite} name="type_of_guest" id="type_1" type="radio" required="required" class="custom-control-input" value={2}> 
-          <label for="type_1" class="custom-control-label">Evening only</label>
-        </div>
-      </div>
-    </div>
-  </div>
   <div class="form-group row">
     <label for="name" class="col-4 col-form-label">Name</label> 
     <div class="col-8">
@@ -56,23 +34,22 @@
       <textarea id="comment" name="comment" cols="40" rows="3" class="form-control"></textarea>
     </div>
   </div>
-  <input id="start-hidden" name="starter" type="hidden" value="">
-    <p>Please fill out one of the below for each guest in your party including yourself</p>
-    {#each Array(guests) as _, i}
-    {#if invite == 1}
-    <div class="form-group row">
-      <div class="col-8">
-        <div class="custom-controls-stacked">
-          <div class="custom-control custom-radio">
-            <input id="guests" type="file" name="myFile" multiple>
-          </div>
-        </div>
+    
+  <div class="form-group row">
+    <label class="col-4">Attending day ceremony</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input bind:checked={invite} name="day" id="day" type="checkbox" class="custom-control-input" value={true}> 
+        <label for="day" class="custom-control-label"></label>
       </div>
     </div>
+  </div>
+    <input type="hidden" name="myFile" multiple>
+    {#if invite}
+      <p>Please upload a clear photo of the menu choices for each guest below</p>
+      <input type="file" name="myFile" multiple>
     {/if}
-    {#if guests > 1}<hr>{/if}
-    <button on:click={addGuest}>Add Guest</button>
-    {/each}
+  
   
   <div class="form-group row">
     <div class="offset-4 col-8">
